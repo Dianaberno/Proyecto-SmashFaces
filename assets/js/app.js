d3f9dc22517a2abe699be66f6c0a75bb9fe247bc
paola <bernodiana@gmail.com>
2017-10-03 17:23:48 -0500

    en proceso de array

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+var imagenes = [
+    "anais.jpg", "andrea.jpg", "berenisse.jpg",
+    "caterina.jpg", "daniela.jpg", "francisca.jpg",
+    "katerine.jpg", "arantza.jpg", "daguiana.jpg",
+    "elizabeth.jpg", "grecia.jpg", "janine.jpg",
+    "michelle.jpg", "rosario.jpg"
+];
+var nombres = [
+    "anais", "andrea", "berenisse",
+    "caterina", "daniela", "francisca",
+    "katerine", "arantza", "daguiana",
+    "elizabeth", "grecia", "janine",
+    "michelle", "rosario"
+];
